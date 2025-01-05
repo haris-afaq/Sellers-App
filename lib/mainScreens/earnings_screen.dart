391679179b0d6360060fa3bef9be87ba39dd4792
haris.afaq <haris.afaq@inara.pk>
2025-01-05 16:50:35 +0500

    initial commit

---
+++ lib/mainScreens/earnings_screen.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../global/global.dart';
+import '../splashScreen/splash_screen.dart';
+
+class EarningsScreen extends StatefulWidget {
+  const EarningsScreen({Key? key}) : super(key: key);
+
+  @override
+  _EarningsScreenState createState() => _EarningsScreenState();
+}
+
+class _EarningsScreenState extends State<EarningsScreen> {
+  double sellerTotalEarnings = 0;
+
+  retrieveSellerEarnings() async {
+    await FirebaseFirestore.instance
+        .collection("sellers")
+        .doc(sharedPreferences!.getString("uid"))
+        .get()
+        .then((snap) {
+      setState(() {
+        sellerTotalEarnings = double.parse(snap.data()!["earnings"].toString());
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    retrieveSellerEarnings();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "€ $sellerTotalEarnings",
+                style: const TextStyle(
+                    fontSize: 80, color: Colors.white, fontFamily: "Signatra"),
+              ),
+              const Text(
+                "Total Earnings",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                  letterSpacing: 3,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 1.5,
+                ),
+              ),
+              const SizedBox(
+                height: 40.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (c) => const MySplashScreen()));
+                },
+                child: Container(
+                  width: 140,
+                  height: 50,
+                  color: Colors.white54,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Back',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
